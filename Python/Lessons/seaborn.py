6dcefaa682281f27e6307b76c1063c5aa2c7f80f
Andrea To Chip <andreatochip@yahoo.com>
2025-11-07 21:43:54 +0800

    Add files via upload

---
+++ Python/Lessons/seaborn.py
@@ -0,0 +1,44 @@
+# Create scatter plot
+g = sns.relplot(x="weight", 
+                y="horsepower", 
+                data=mpg,
+                kind="scatter")
+
+# Add a title "Car Weight vs. Horsepower"
+g.fig.suptitle("Car Weight vs. Horsepower")
+
+# Show plot
+plt.show()
+
+# Set the style to "darkgrid"
+sns.set_style("darkgrid")
+
+# Set a custom color palette
+# Set a custom color palette
+sns.set_palette(["#39A7D0", "#36ADA4"])
+
+'''
+'''
+
+# Create the box plot of age distribution by gender
+sns.catplot(x="Gender", y="Age", 
+            data=survey_data, kind="box")
+
+# Show plot
+plt.show()
+
+'''
+'''
+
+# Create a point plot that uses color to create subgroups
+g = sns.catplot(kind="point",data=student_data,x="romantic",y="absences",hue="school")
+
+# Add a title "Average MPG Over Time"
+g.set_title("Average MPG Over Time")
+
+# Add x-axis and y-axis labels
+g.set(xlabel="Car Model Year",ylabel="Average MPG")
+
+
+# Show plot
+plt.show()
