6dc8abaedc3565749b3b1c4d4d1c2f85e1d6b9b2
Andrea To Chip <andreatochip@yahoo.com>
2025-11-08 17:38:19 +0800

    Update datasets_exploratory.py

--- Python/Lessons/datasets_exploratory.py
+++ Python/Lessons/datasets_exploratory.py
@@ -42,3 +42,77 @@ plt.show()
 
 '''
 '''
+
+# Count the number of missing values in each column
+print(planes.isna().sum())
+
+# Find the five percent threshold
+threshold = len(planes) * 0.05
+
+# Create a filter
+cols_to_drop = planes.columns[planes.isna().sum() <= threshold]
+
+# Drop missing values for columns below the threshold
+planes.dropna(subset=cols_to_drop, inplace=True)
+
+print(planes.isna().sum())
+
+'''
+'''
+
+# Calculate median plane ticket prices by Airline
+airline_prices = planes.groupby("Airline")["Price"].median()
+
+print(airline_prices)
+
+# Convert to a dictionary
+prices_dict = airline_prices.to_dict()
+
+# Map the dictionary to the missing values
+planes["Price"] = planes["Price"].fillna(planes["Airline"].map(prices_dict))
+
+# Check for missing values
+print(planes.isna().sum())
+
+'''
+'''
+
+# Create a list of categories
+flight_categories = ["Short-haul", "Medium", "Long-haul"]
+
+# Create short-haul values
+short_flights = "^0h|^1h|^2h|^3h|^4h"
+
+# Create medium-haul values
+medium_flights = "^5h|^6h|^7h|^8h|^9h"
+
+# Create long-haul values
+long_flights = "10h|^11h|^12h|^13h|^14h|^15h|^16h"
+
+# Create conditions for values in flight_categories to be created
+conditions = [
+    (planes["Duration"].str.contains(short_flights)),
+    (planes["Duration"].str.contains(medium_flights)),
+    (planes["Duration"].str.contains(long_flights))
+]
+
+# Apply the conditions list to the flight_categories
+planes["Duration_Category"] = np.select(conditions, 
+                                        flight_categories,
+                                        default="Extreme duration")
+
+# Plot the counts of each category
+sns.countplot(data=planes, x="Duration_Category")
+plt.show()
+
+# Price standard deviation by Airline
+planes["airline_price_st_dev"] = planes.groupby("Airline")["Price"].transform(lambda x: x.std())
+print(planes[["Airline", "airline_price_st_dev"]].value_counts())
+
+# Median Duration by Airline
+planes["airline_median_duration"] = planes.groupby("Airline")["Duration"].transform(lambda x: x.median())
+print(planes[["Airline","airline_median_duration"]].value_counts())
+
+# Mean Price by Destination
+planes["price_destination_mean"] = planes.groupby("Destination")["Price"].transform(lambda x: x.mean())
+print(planes[["Destination","price_destination_mean"]].value_counts())
