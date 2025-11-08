51ca5911ab8a775edff382184d8b1af953693268
Andrea To Chip <andreatochip@yahoo.com>
2025-11-08 16:35:06 +0800

    Update datasets_exploratory.py

--- Python/Lessons/datasets_exploratory.py
+++ Python/Lessons/datasets_exploratory.py
@@ -1,1 +1,10 @@
+# Update the data type of the 2019 column to a float
+unemployment["2019"] = unemployment["2019"].astype(float)
+# Print the dtypes to check your work
+print(unemployment.dtypes)
 
+# Define a Series describing whether each continent is outside of Oceania
+not_oceania = ~unemployment["continent"].isin(["Oceania"])
+
+# Print unemployment without records related to countries in Oceania
+print(unemployment[not_oceania])
