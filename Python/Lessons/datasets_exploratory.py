becc1eddfff89cb48dc2222a5526ef8fe28d4ba0
Andrea To Chip <andreatochip@yahoo.com>
2025-11-08 16:40:29 +0800

    Update datasets_exploratory.py

--- Python/Lessons/datasets_exploratory.py
+++ Python/Lessons/datasets_exploratory.py
@@ -8,3 +8,25 @@ not_oceania = ~unemployment["continent"].isin(["Oceania"])
 
 # Print unemployment without records related to countries in Oceania
 print(unemployment[not_oceania])
+
+# Print the minimum and maximum unemployment rates during 2021
+print(unemployment["2021"].min(), unemployment["2021"].max())
+
+# Create a boxplot of 2021 unemployment rates, broken down by continent
+sns.boxplot(x='2021',y='continent',data=unemployment)
+plt.show()
+
+# Print mean and standard deviation grouped by continent
+print(unemployment[["continent", "2019", "2020"]].groupby("continent").agg(["mean", "std"]))
+
+continent_summary = unemployment.groupby("continent").agg(
+    # Create the mean_rate_2021 column
+    mean_rate_2021=("2021", "mean"),
+    # Create the std_rate_2021 column
+    std_rate_2021=("2021", "std")
+)
+print(continent_summary)
+
+# Create a bar plot of continents and their average unemployment
+sns.barplot(x='continent', y='2021', data=unemployment)
+plt.show()
